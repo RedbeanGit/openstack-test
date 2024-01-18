07ff2e0088cadafdf92da7660b5f6410d0f79379
RedbeanGit <dubois.julien.mail@gmail.com>
2024-01-18 11:51:40 +0100

    fix: kubernetes with tf missing provider
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- kubernetes/kubernetes.tf
+++ kubernetes/kubernetes.tf
@@ -1,4 +1,4 @@
-resource "k0s_cluster" "example" {
+resource "k0s_cluster" "kubernetes_cluster" {
   name    = "kubernetes-cluster"
   version = "1.28.5+k0s.0"
 
@@ -7,20 +7,18 @@ resource "k0s_cluster" "example" {
       role = "controller"
 
       ssh = {
-        address  = openstack_networking_floatingip_v2.master_floating_ip[i].address
-        port     = 22
-        user     = "root"
-        key_path = var.ssh_key_path
+        address = openstack_networking_floatingip_v2.master_floating_ip[i].address
+        port    = 22
+        user    = "root"
       }
     }],
     [for i in range(var.nb_worker) : {
       role = "worker"
 
       ssh = {
-        address  = openstack_networking_floatingip_v2.worker_floating_ip[i].address
-        port     = 22
-        user     = "root"
-        key_path = var.ssh_key_path
+        address = openstack_networking_floatingip_v2.worker_floating_ip[i].address
+        port    = 22
+        user    = "root"
       }
     }]
   )
